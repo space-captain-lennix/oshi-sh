ed927cd9f936f7ccd7894da56e30c0825a703336
Lennix <space.captain@lennix.be>
2022-10-31 21:43:35 +0100

    initial commit

---
+++ src/main/java/be/lennix/oshish/OshiShApplication.java
@@ -0,0 +1,13 @@
+package be.lennix.oshish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshiShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshiShApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/lennix/oshish/OshiShApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lennix.oshish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshiShApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
